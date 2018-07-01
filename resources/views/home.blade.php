af39a1c6ecbf3cbade7e2c7bcdd90531102fabd6
drakedey <joskarandres97@example.com>
2018-07-01 05:21:42 -0700

    Cart added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,70 +16,57 @@
         </div>	 
 
         <br>
-<div class="container">
+<div class="container a">
         <div class="row">
-            <div class="card-deck">
-        @foreach($Articles as $Article)
-        <div class="col-lg-3">
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{ asset("img/albums/".$Article->img_route.".png") }}" alt="Card image cap">
-            <div class="card-body">
-            <h5 class="card-title">{{$Article->name}}</h5>
-            <p class="card-text">{{$Article->description}}</p>
-            
-            @guest
-                 
-            @else
-              <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal{{$Article->id}}">Add to the cart!</a>
-              @endguest
-            </div>
-            <div class="card-footer">
-                <small class="text-muted"></small>
-              </div>
-          </div>
-        </div>
-    </div>
-
-
-    <div class="modal fade" id='exampleModal{{$Article->id}}'  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content modalback">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add!</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-        <div class="row">
-            <div class= "col-6" >
-            <img class="card-img-top" src="{{ asset("img/albums/".$Article->img_route.".png") }}" alt="Card image cap">
-       </div>
-       <div class= "col-6" >Name: {{$Article->name}} <br> Price: {{$Article->price}} $ <br> Existencia: {{$Article->exist}}
-       {!! Form::open(['url'=>'/create_purchase']) !!}
-       {!! Form::label('cantidad', 'Cantidad') !!}
-       {!! Form::number('cantidad', '1', ['class'=>'form-control', 'min'=>'1', 'max'=>$Article->exist]) !!}
-       {!! Form::hidden('user_id',Auth::user()->id) !!}
-       {!! Form::hidden('art_id',$Article->id) !!}
-       {!! Form::submit('Add', ['class'=>'btn btn-primary']) !!}
-       {!! Form::close() !!}
-                
-        </div>
-
-  </div>
-      <div class="modal-footer">
+            @foreach($Articles as $Article)
+            <div class="col-md-4">
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="{{ asset("img/albums/".$Article->img_route.".png") }}" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{$Article->name}}</h5>
+                  <p class="card-text">{{$Article->description}}</p>
+                  @guest
+                  @else
+                  <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal{{$Article->id}}">Add to the cart!</a>
+                  @endguest
+                </div>
+              </div>  
+            </div> 
         
-
-      </div>
-    </div>
-  </div>
-</div>
-@endforeach
-
+            <div class="modal fade" id='exampleModal{{$Article->id}}'  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content modalback">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Add!</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="row">
+                          <div class= "col-6" >
+                            <img class="card-img-top" src="{{ asset("img/albums/".$Article->img_route.".png") }}" alt="Card image cap">
+                          </div>
+                          <div class= "col-6" >Name: {{$Article->name}} <br> Price: {{$Article->price}} $ <br> Existencia: {{$Article->exist}}
+                            {!! Form::open(['url'=>'/create_purchase']) !!}
+                            {!! Form::label('cantidad', 'Cantidad') !!}
+                            {!! Form::number('cantidad', '1', ['class'=>'form-control', 'min'=>'1', 'max'=>$Article->exist]) !!}
+                            {!! Form::hidden('user_id',Auth::user()->id) !!}
+                            {!! Form::hidden('art_id',$Article->id) !!}
+                            {!! Form::submit('Add', ['class'=>'btn btn-primary']) !!}
+                            {!! Form::close() !!}
+                          </div>
+                       </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+          @endforeach
         </div>
+      </div>
 
-    </div>
+
+
         <script type="text/javascript" src="{{ asset('engine1/wowslider.js') }}" defer ></script>
         <script type="text/javascript" src="{{ asset('engine1/script.js') }}" defer></script>
 
